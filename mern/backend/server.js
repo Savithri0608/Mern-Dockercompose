9f8c3add42d4428eeea4faeefb30ab1971bada2e
Savithri Marati <savithri62001@gmail.com>
2025-06-04 10:59:43 +0530

    Update server.js

--- mern/backend/server.js
+++ mern/backend/server.js
@@ -4,10 +4,18 @@ import records from "./routes/record.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+const cors = require('cors');
+const express = require('express');
+const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://52.23.232.26:5173',  // Replace with actual IP or domain
+  credentials: true,                     // If using cookies or sessions
+}));
+
 app.use(express.json());
 app.use("/record", records);
+
 
 // start the Express server
 app.listen(5050, '0.0.0.0', () => {
